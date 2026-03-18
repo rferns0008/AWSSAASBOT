a76e634d5d9cbcab66567387a2dba8d50575123e
Rahul Fernandes <fernandes.r2001@gmail.com>
2026-03-18 19:01:32 +0530

    Updated files to reflect latest configuration changes and added required dependencies in requirements.txt.

--- main.tf
+++ main.tf
@@ -111,10 +111,10 @@ module "eks" {
   eks_managed_node_groups = {
     testing_nodes = {
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 2
+      desired_size = 1
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.small"]
       capacity_type  = "SPOT"
       
       # Keep any existing custom tags or configurations you had here
