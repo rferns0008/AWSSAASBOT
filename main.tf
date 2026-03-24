1b778f728fc6d61032192cacf0aca164e9c65e7d
Rahul Fernandes <fernandes.r2001@gmail.com>
2026-03-24 17:31:02 +0530

    Updated alb_dns_name value in main.tf.

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
 
 variable "alb_dns_name" {
   type    = string
-  default = "adc1793e274914bafbbb64cf526ff4ac-1914326458.ap-south-1.elb.amazonaws.com"
+  default = "dualstack.k8s-chatbotp-flaskcha-c015bdd713-1037106485.ap-south-1.elb.amazonaws.com."
 }
 
 locals {
@@ -213,21 +213,18 @@ resource "aws_s3_bucket" "frontend" {
   force_destroy = true 
 }
 
-# PATCH: Added Bucket Policy to allow CloudFront OAC Access
+# NEW: This policy allows CloudFront to reach into your private S3 bucket
 resource "aws_s3_bucket_policy" "frontend_policy" {
   bucket = aws_s3_bucket.frontend.id
-
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Sid       = "AllowCloudFrontServicePrincipalReadOnly"
         Effect    = "Allow"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.frontend.arn}/*"
+        Principal = { Service = "cloudfront.amazonaws.com" }
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
         Condition = {
           StringEquals = {
             "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
@@ -251,8 +248,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
     origin_id                = "S3-Frontend"
   }
-  
-  # ... (Rest of your CloudFront config remains exactly the same)
   enabled             = true
   default_root_object = "index.html"
   aliases             = [local.domain_name]
@@ -268,11 +263,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       cookies { forward = "none" }
     }
   }
-  
   restrictions {
     geo_restriction { restriction_type = "none" }
   }
-  
   viewer_certificate {
     acm_certificate_arn      = local.certificate_arn
     ssl_support_method       = "sni-only"
@@ -314,7 +307,7 @@ resource "aws_route53_record" "api" {
   allow_overwrite = true
   alias {
     name                   = var.alb_dns_name
-    zone_id                = "ZP97RAFLXTNZK"
+    zone_id                = "ZP97RAFLXTNZK" # Always use this ID for ALBs in ap-south-1
     evaluate_target_health = true
   }
 }
